0757817ae543e9938202bdb907dbe871a068deb3
hristinamilkic <milkichristina@gmail.com>
2025-06-14 23:10:54 +0200

    rm user role imports and type

--- server/src/interfaces/user.interface.ts
+++ server/src/interfaces/user.interface.ts
@@ -1,12 +1,10 @@
 import { Document } from "mongoose";
-import { UserRole } from "../enums/user.roles";
 
 export interface IUser extends Document {
   email: string;
   password: string;
   firstName: string;
   lastName: string;
-  role: UserRole;
   isActive: boolean;
   isPremium: boolean;
   isAdmin: boolean;
